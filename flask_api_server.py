c3e336e3c54e3b9ea66ebea1df39c90046f6de68
Hesham87ls <heshammedhat87@gmail.com>
2024-03-24 01:35:11 +0200

    first submission before testing

--- flask_api_server.py
+++ flask_api_server.py
@@ -10,94 +10,214 @@ from luma.core.render import canvas
 from luma.led_matrix.device import apa102
 from luma.core.interface.serial import spi, bitbang
 
+import threading
+
+import pigpio
+from time import sleep
 
 app = Flask(__name__)
 api = Api(app)
 
-# Number of LED's in your APA102 LED Strip.
-NUM_LEDS = 150
+logger = logging.getLogger('APA102')
+class APA102():
+    def __init__(self, NUM_LEDS = 150, rainbow = False, contrast_level = 128):
 
-# Color buffer "array", initialised to all black.
-# The color values in this buffer are applied to the APA102 LED Strip
-# by the update() function. The value at position 0 = the LED first LED
-# in the strip.
-color_buffer = deque(['black']*NUM_LEDS, maxlen=NUM_LEDS)
+        # Number of LED's in your APA102 LED Strip.
+        self.NUM_LEDS = NUM_LEDS
 
-# Initialise serial using Hardware SPI0 (SCLK=BCM 11, MOSI/SDA=BCM 10).
-# Default bus_speed_hz=8000000. This value may need to be lowered
-# if your Logic Level Converter cannot switch fast enough.
-# Allowed values are 500000, 1000000, 2000000, 4000000, 8000000, 16000000, 32000000
-# For find the spi class at https://github.com/rm-hull/luma.core/blob/master/luma/core/interface/serial.py
-serial = spi(port=0, device=0, bus_speed_hz=2000000)
+        # Color buffer "array", initialised to all black.
+        # The color values in this buffer are applied to the APA102 LED Strip
+        # by the update() function. The value at position 0 = the LED first LED
+        # in the strip.
+        self.color_buffer = deque(['black']*NUM_LEDS, maxlen=NUM_LEDS)
 
-# Initialise serial using "Big Banging" SPI technique on general GPIO Pins.
-# For find the bitbang class at https://github.com/rm-hull/luma.core/blob/master/luma/core/interface/serial.py
-# serial = bitbang(SCLK=13, SDA=6)
+        # Initialise serial using Hardware SPI0 (SCLK=BCM 11, MOSI/SDA=BCM 10).
+        # Default bus_speed_hz=8000000. This value may need to be lowered
+        # if your Logic Level Converter cannot switch fast enough.
+        # Allowed values are 500000, 1000000, 2000000, 4000000, 8000000, 16000000, 32000000
+        # For find the spi class at https://github.com/rm-hull/luma.core/blob/master/luma/core/interface/serial.py
+        self.serial = spi(port=0, device=0, bus_speed_hz=2000000)
 
-#Initialise APA102 device instance using serial instance created above.
-device = apa102(serial_interface=serial, cascaded=NUM_LEDS)
+        # Initialise serial using "Big Banging" SPI technique on general GPIO Pins.
+        # For find the bitbang class at https://github.com/rm-hull/luma.core/blob/master/luma/core/interface/serial.py
+        # serial = bitbang(SCLK=13, SDA=6)
 
-# Reset device and set it's global contrast level.
-device.clear()
-contrast_level = 128 # 0 (off) to 255 (maximum brightness)
-device.contrast(contrast_level)
+        #Initialise APA102 device instance using serial instance created above.
+        self.device = apa102(serial_interface=self.serial, cascaded=NUM_LEDS)
 
-rainbow = False
+        # Reset device and set it's global contrast level.
+        self.device.clear()
 
+        self.device.contrast(contrast_level)   # 0 (off) to 255 (maximum brightness)
 
-def set_color(color='black', index=-1):                                                # (8)
-    """
-    Set the color of single LED (index >= 0), or all LEDs (when index == -1)
-    """
-    if index == -1:
-        global color_buffer
-        color_buffer = deque([color]*NUM_LEDS, maxlen=NUM_LEDS)
-    else:
-        color_buffer[index] = color
+        self.rainbow = rainbow
 
 
-def update():                                                                           # (12)
-    """
-    Apply the color buffer to the APA102 strip.
-    """
-    with canvas(device) as draw:
-        for led_pos in range(0, len(color_buffer)):
-            color = color_buffer[led_pos]
-
-            ## If your LED strip's colors are are not in the expected
-            ## order, uncomment the following lines and adjust the indexes
-            ## in the line color = (rgb[0], rgb[1], rgb[2])
-            # rgb = getrgb(color)
-            # color = (rgb[0], rgb[1], rgb[2])
-            # if len(rgb) == 4:
-            #     color += (rgb[3],)  # Add in Alpha
-
-            draw.point((led_pos, 0), fill=color)
+    def set_contrast(self, contrast):
+        self.device.contrast(contrast)
 
 
-def push_color(color):                                                                  # (9)
-    """
-    Push a new color into the color array at index 0. The last value is dropped.
-    """
-    color_buffer.appendleft(color)
+    def set_color(self, color='black', index=-1):                                                # (8)
+        """
+        Set the color of single LED (index >= 0), or all LEDs (when index == -1)
+        """
+        if index == -1:
+            self.color_buffer = deque([color]*self.NUM_LEDS, maxlen=self.NUM_LEDS)
+        else:
+            self.color_buffer[index] = color
+
+    def update(self):                                                            
+        """
+        Apply the color buffer to the APA102 strip.
+        """
+        with canvas(self.device) as draw:
+            for led_pos in range(0, len(self.color_buffer)):
+                color = self.color_buffer[led_pos]
+
+                ## If your LED strip's colors are are not in the expected
+                ## order, uncomment the following lines and adjust the indexes
+                ## in the line color = (rgb[0], rgb[1], rgb[2])
+                # rgb = getrgb(color)
+                # color = (rgb[0], rgb[1], rgb[2])
+                # if len(rgb) == 4:
+                #     color += (rgb[3],)  # Add in Alpha
+
+                draw.point((led_pos, 0), fill=color)
 
 
-def rainbow_example(rounds=1, delay_secs=0.01):
-    """
-    Rainbow sequence animation example.
-    """
-    set_color('black') # Start with all LED's "off"
-    update()
+    def push_color(self, color):                                                                  # (9)
+        """
+        Push a new color into the color array at index 0. The last value is dropped.
+        """
+        self.color_buffer.appendleft(color)
 
-    saturation = 100 # 0 (grayer) to 100 (full color)
-    brightness = 100 # 0 (darker) to 100 (brighter)
 
-    for i in range(0, rounds):
-        for hue in tuple(range(0, 360)) + tuple(range(360, -1, -1)): # 0..360..0
-            color_str = "hsb({}, {}%, {}%)".format(hue, saturation, brightness)
-            push_color(color_str)
-            update()
-            sleep(delay_secs)
+
+
+
+class APA102Control(Resource):
+    PRESSED  = "PRESSED"
+    RELEASED = "RELEASED"
+    HOLD     = "HOLD"
+    def __init__(self, apa, gpio, pi, amplitude_file, max_amplitude, hold_secs=0.5, delay_secs=0.25, callback=None, color = 'red'):
+        """ Constructor """
+
+        self.args_parser = reqparse.RequestParser()
+
+        self.args_parser.add_argument(
+            name = "Rainbow",
+            required = True,
+            type = inputs.boolean(),
+            help = 'make rainbow effect {error_msg}',
+            default=False)
+        
+        self.gpio = gpio
+        self.pi = pi
+        self.hold_secs = hold_secs
+        self.callback = callback
+
+        self.rainbow = False
+        
+        self.color = color
+        # Setup Button GPIO as INPUT and enable internal Pull-Up Resistor.
+        # Our button is therefore Active LOW.
+        self.pi.set_mode(gpio, pigpio.INPUT)
+        self.pi.set_pull_up_down(gpio, pigpio.PUD_UP)
+        self.pi.set_glitch_filter(gpio, 10000) # microseconds debounce
+
+        self._hold_timer = 0  # For detecting hold events.
+        self.pressed = False  # True when button pressed, false when released.
+        self.hold = False     # Hold has been detected.
+
+        # Register internal PiGPIO callback (as an alternative to polling the button in a while loop)
+        self._pigpio_callback = self.pi.callback(self.gpio, pigpio.EITHER_EDGE, self.rainbow_toggel)
+
+        self.amplitude_file = amplitude_file
+        self.max_amplitude = max_amplitude
+
+
+        self.apa = apa
+        APA102.set_color(self.apa, color=self.color)
+        APA102.update(self.apa)
+
+
+        self.is_polling = False
+        self.delay_secs = delay_secs
+
+        self._thread = None
+        self._start() # Start polling ADC
+
+
+    def rainbow_toggel(self):
+        self.rainbow = not self.rainbow
+        logger.info("rainbow is set to {}".format(self.rainbow))
+        self.rainbow_lights()
+
+    def get_rainbow(self):
+        # return true if rainbow is activated
+        return self.rainbow
+    
+    def rainbow_lights(self, delay_secs=0.01):
+        """
+        Rainbow sequence animation example.
+        """
+        APA102.set_color(self.apa, 'black') # Start with all LED's "off"
+        APA102.update(self.apa)
+
+        saturation = 100 # 0 (grayer) to 100 (full color)
+        brightness = 100 # 0 (darker) to 100 (brighter)
+
+        while(self.rainbow):
+            for hue in tuple(range(0, 360)) + tuple(range(360, -1, -1)): # 0..360..0
+                color_str = "hsb({}, {}%, {}%)".format(hue, saturation, brightness)
+                APA102.set_color(self.apa, color_str)
+                APA102.update(self.apa)
+                sleep(delay_secs)
+
+    def post(self):
+        args = self.args_parser.parse_args()
+
+        self.rainbow = args.Rainbow
+
+
+    def __str__(self):
+        """ To String """
+        return "Rainbow value is {}".format(self.rainbow)
+
+
+    def run(self):
+        """ Poll ADC for Voltage Changes """
+        while self.is_polling:                                                           # (1)
+            with open(self.amplitude_file, 'r') as f:
+                amplitudes = f.readlines()
+                for amplitude in amplitudes:
+                    APA102.set_contrast((float(amplitude)/self.max_amplitude) * 255)
+                    sleep(self.delay_secs)
+            
+
+        # self.is_polling has become False and the Thread ends.
+        self._thread = None
+        logger.debug("Potentiometer Polling Thread Finished.")
+
+
+    def _start(self):
+        """ Start Polling ADC """
+
+        if self._thread is not None:
+            # Thread already exists.
+            logger.warn("Polling Thread Already Started.")
+            return
+
+        self._thread = threading.Thread(name='Potentiometer',
+                                        target=self.run,
+                                        daemon=True)
+
+        self.is_polling = True
+        self._thread.start()
+        logger.debug("Potentiometer Polling Thread Started.")
+
+# Register Flask-RESTful resource and mount to server end point /led
+api.add_resource(APA102Control, '/APA102')
 
 
 # @app.route applies to the core Flask instance (app).
@@ -107,34 +227,3 @@ def index():
     """Make sure inde.html is in the templates folder
     relative to this Python file."""
     return render_template('index_api_client.html')
-
-
-# Flask-restful resource definitions.
-# A 'resource' is modeled as a Python Class.
-class LEDControl(Resource):                
-    def __init__(self):
-        self.args_parser = reqparse.RequestParser()                                  # (11)
-
-        self.args_parser.add_argument(
-            name = "Rainbow",
-            required = True,
-            type = inputs.boolean(),
-            help = 'make rainbow effect {error_msg}',
-            default=False)
-        
-    def get(self):
-        global rainbow
-        # return true if rainbow is activated
-        return rainbow
-    
-    def post(self):
-        global rainbow
-
-        args = self.args_parser.parse_args()
-
-        rainbow = args.Rainbow
-
-
-
-# Register Flask-RESTful resource and mount to server end point /led
-api.add_resource(LEDControl, '/rainbow')
---
+++ main.py
@@ -0,0 +1,54 @@
+from pydub import AudioSegment
+import numpy as np
+from flask_api_server import APA102, APA102Control
+import pigpio
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("Main")
+
+def song_to_amplitude_intervals(audio_path, interval_duration_ms=250):
+    # Load the song
+    song = AudioSegment.from_mp3(audio_path)
+    
+    # Split the song into chunks of 0.25 seconds
+    interval_duration = interval_duration_ms  # in milliseconds
+    chunks = [song[i:i+interval_duration] for i in range(0, len(song), interval_duration)]
+    
+    # Calculate the average amplitude for each chunk
+    average_amplitudes = []
+    max_amplitude = 0.0
+    for chunk in chunks:
+        # Get the raw audio data as a numpy array
+        samples = np.array(chunk.get_array_of_samples())
+        # Calculate the average amplitude for the chunk
+        avg_amplitude = np.mean(np.abs(samples))
+        if max_amplitude < avg_amplitude:
+            max_amplitude = avg_amplitude
+        average_amplitudes.append(avg_amplitude)
+    
+    return average_amplitudes, max_amplitude
+
+def save_amplitudes_to_file(amplitudes, output_file):
+    with open(output_file, 'w') as f:
+        for amplitude in amplitudes:
+            f.write(f"{amplitude}\n")
+
+
+BUTTON_GPIO = 21
+
+pi = pigpio.pi()
+
+# Example usage
+if __name__ == "__main__":
+    audio_path = 'test-tube-194556.mp3'
+    output_file = 'amplitude_output.txt'
+    amplitudes, max_amplitude = song_to_amplitude_intervals(audio_path)
+    save_amplitudes_to_file(amplitudes, output_file)
+
+    apa = APA102()
+
+    def button_handler():
+        logger.info("Button is pressed")
+
+    musical_lights = APA102Control(apa, gpio=BUTTON_GPIO, pi=pi, amplitude_file=output_file, max_amplitude=max_amplitude, callback=button_handler)
